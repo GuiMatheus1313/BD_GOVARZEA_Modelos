f8dde0c0fc9bb170310c86a7fcbaa7726eb38d43
GuiMatheus1313 <guilhermematheusdejesusaraujo6@gmail.com>
2023-12-04 18:48:38 -0300

    Correção com a adição do Juiz

--- Físico/schema.sql
+++ Físico/schema.sql
@@ -1,7 +1,5 @@
 create database GoVarzea_bd;
 
-<!--drop database GoVarzea_bd; -->
-
 use GoVarzea_bd;
 
 create table Estado(
@@ -25,9 +23,8 @@ create table Endereco(
 );
 
 create table Calendario(
-	codigo int identity,
-	data_calendario datetime,
-	constraint pk_codigo_data primary key(codigo, data_calendario)
+	codigo int identity primary key,
+	data_calendario datetime not null
 );
 
 create table Prioridade_evento(
@@ -45,13 +42,15 @@ create table Pessoas(
 create table Pessoas_telefone(
 	fk_cpf bigint,
 	telefone bigint,
-	constraint fkpk_cpf_telefone primary key(fk_cpf, telefone)
+	constraint fkpk_cpf_telefone primary key(fk_cpf, telefone),
+	constraint fk_cpf foreign key(fk_cpf) references Pessoas(CPF)
 );
 
 create table Pessoas_email(
 	fk_cpf bigint,
 	email varchar(50),
-	constraint fkpk_cpf_email primary key(fk_cpf, email)
+	constraint fkpk_cpf_email primary key(fk_cpf, email),
+	constraint fk_cpf_pessoa foreign key(fk_cpf) references Pessoas(CPF)
 );
 
 create table Evento(
@@ -61,9 +60,15 @@ create table Evento(
 	constraint fk_prioridade foreign key(fk_prioridade) references Prioridade_evento(codigo)
 );
 
+create table Juiz(
+	fk_cpf bigint primary key,
+	geolocalizacao varchar(100) not null
+	constraint fk_cpf7 foreign key(fk_cpf) references Pessoas(CPF)
+);
+
 create table Dirigente(
 	fk_cpf bigint primary key
-	constraint fk_cpf foreign key(fk_cpf) references Pessoas(CPF)
+	constraint fk_cpf2 foreign key(fk_cpf) references Pessoas(CPF)
 );
 
 create table Jogador_posicao(
@@ -74,24 +79,24 @@ create table Jogador_posicao(
 create table Jogador(
 	fk_cpf bigint primary key,
 	fk_posicao int not null, 
-	constraint fk_cpf foreign key(fk_cpf) references Pessoas(CPF),
+	constraint fk_cpf3 foreign key(fk_cpf) references Pessoas(CPF),
 	constraint fk_posicao foreign key(fk_posicao) references Jogador_posicao(codigo)
 );
 
 create table Torcedor(
 	fk_cpf bigint primary key
-	constraint fk_cpf foreign key(fk_cpf) references Pessoas(CPF)
+	constraint fk_cpf4 foreign key(fk_cpf) references Pessoas(CPF)
 );
 
 create table Dono_comercio(
 	fk_cpf bigint primary key,
-	geolocalizacao varchar not null
-	constraint fk_cpf foreign key(fk_cpf) references Pessoas(CPF)
+	geolocalizacao varchar(100) not null
+	constraint fk_cpf5 foreign key(fk_cpf) references Pessoas(CPF)
 );
 
 create table Patrocinador(
-	fk_cpf bigint primary key
-	constraint fk_cpf foreign key(fk_cpf) references Pessoas(CPF)
+	fk_cpf bigint primary key,
+	constraint fk_cpf6 foreign key(fk_cpf) references Pessoas(CPF)
 );
 
 create table Empresa(
@@ -120,7 +125,7 @@ create table Amistoso(
 
 create table Liga(
 	codliga int identity primary key,
-	constraint fk_codamistoso foreign key(codliga) references Evento(codigo)
+	constraint fk_codliga foreign key(codliga) references Evento(codigo)
 );
 
 create table Copa(
@@ -143,7 +148,7 @@ create table Campos(
 	fk_amistoso int null,
 	fk_liga int null,
 	fk_copa int null
-	constraint fk_endereco foreign key(fk_endereco) references Endereco(CEP),
+	constraint fk_endereco2 foreign key(fk_endereco) references Endereco(CEP),
 	constraint fk_associacao foreign key(fk_associacao) references Associacao(codigo),
 	constraint fk_amistoso foreign key(fk_amistoso) references Amistoso(codamistoso),
 	constraint fk_liga foreign key(fk_liga) references Liga(codliga),
@@ -159,11 +164,11 @@ create table Time(
 	fk_amistoso int null, 
 	fk_liga int null, 
 	fk_copa int null,
-	constraint fk_associacao foreign key(fk_associacao) references Associacao(codigo),
-	constraint fk_patrocinador foreign key(fk_patrocinador) references Patrocinador(fk_cpf),
-	constraint fk_amistoso foreign key(fk_amistoso) references Amistoso(codamistoso),
-	constraint fk_liga foreign key(fk_liga) references Liga(codliga),
-	constraint fk_copa foreign key(fk_copa) references Copa(codcopa)
+	constraint fk_associacao2 foreign key(fk_associacao) references Associacao(codigo),
+	constraint fk_patrocinador2 foreign key(fk_patrocinador) references Patrocinador(fk_cpf),
+	constraint fk_amistoso2 foreign key(fk_amistoso) references Amistoso(codamistoso),
+	constraint fk_liga2 foreign key(fk_liga) references Liga(codliga),
+	constraint fk_copa2 foreign key(fk_copa) references Copa(codcopa)
 );
 
 create table Letra_divisao(
@@ -172,8 +177,8 @@ create table Letra_divisao(
 );
 
 create table Time_divisoes(
-	fk_codtime int not null, 
-	fk_divisao int not null,
+	fk_codtime int, 
+	fk_divisao int,
 	constraint fkpk_codtime_divisao primary key(fk_codtime, fk_divisao),
 	constraint fk_codtime foreign key(fk_codtime) references Time(codigo),
 	constraint fk_divisao foreign key(fk_divisao) references Letra_divisao(codigo)
@@ -182,6 +187,50 @@ create table Time_divisoes(
 create table Jogador_time(
 	fk_codtime int not null,
 	fk_jogador bigint not null
-	constraint fk_codtime foreign key(fk_codtime) references Time(codigo),
+	constraint fkpk_codtime_jogador primary key(fk_codtime, fk_jogador),
+	constraint fk_codtime2 foreign key(fk_codtime) references Time(codigo),
 	constraint fk_jogador foreign key(fk_jogador) references Jogador(fk_cpf),
+);
 
+create table Jogos(
+	codigo int identity primary key,
+	fk_associacao int not null,
+	fk_calendario int not null,
+	fk_evento int not null,
+	constraint fk_associacao3 foreign key(fk_associacao) references Associacao(codigo),
+	constraint fk_calendario foreign key(fk_calendario) references Calendario(codigo),
+	constraint fk_evento foreign key(fk_evento) references Evento(codigo)
+);
+
+create table Jogos_time(
+	fk_jogo int, 
+	fk_time int,
+	constraint fkpk_jogo_time primary key(fk_jogo, fk_time),
+	constraint fk_jogo foreign key(fk_jogo) references Jogos(codigo),
+	constraint fk_time foreign key(fk_time) references Time(codigo)
+);
+
+create table Dirigentes_ligas(
+	CPF_dirigente bigint,
+	fk_liga int,
+	constraint fkpk_dirigente_liga primary key(CPF_dirigente, fk_liga),
+	constraint fk_dirigente foreign key(CPF_dirigente) references Dirigente(fk_cpf),
+	constraint fk_liga3 foreign key(fk_liga) references Liga(codliga)
+);
+
+create table Dirigentes_copas(
+	CPF_dirigente bigint,
+	fk_copa int,
+	constraint fkpk_dirigente_copa primary key(CPF_dirigente, fk_copa),
+	constraint fk_dirigente2 foreign key(CPF_dirigente) references Dirigente(fk_cpf),
+	constraint fk_copa3 foreign key(fk_copa) references Copa(codcopa)
+);
+
+
+create table Pessoas_torcedoras(
+	fk_pessoa bigint,
+	fk_time int,
+	constraint fkpk_pessoa_time primary key(fk_pessoa, fk_time),
+	constraint fk_pessoa foreign key(fk_pessoa) references Pessoas(CPF),
+	constraint fk_time3 foreign key(fk_time) references Time(codigo)
+);
