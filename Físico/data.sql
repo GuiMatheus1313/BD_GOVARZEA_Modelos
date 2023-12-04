d86c54cb13c20fe1e34c1f42bec82939b6550101
GuiMatheus1313 <guilhermematheusdejesusaraujo6@gmail.com>
2023-12-03 21:43:17 -0300

    ADD exemplos de dados "reais" para inserções

---
+++ Físico/data.sql
@@ -0,0 +1,39 @@
+use GoVarzea_bd
+
+/*
+DADOS EXEMPLOS PARA UTILIZAR
+
+Rua Benedito Pereira 135
+Rua Antônio Biachi 135
+Rua Doutor Arroy 135
+Avenida Mutinga, 93
+Avenida Edgar Facó, 100
+62 Rua Jannart Moutinho Ribeiro
+1197431-6789
+2298743-7980
+Avenida Raimundo Pereira de Magalhães, 3386
+Avenida Mutinga, 951
+Rua Doutor Joy Arruda 246
+Rua Doutor Manuel D'Elboux 91
+Associação RalaPoeira, hgitjtir.jpg, azul-claro, amarelo-escuro
+Associação ColinaSuperior, ttdgttff.jpg, vermelho, preto
+Associação SalGrosso, jgytef.jpg, branca, preta
+Time 100Sal, vfdvrefe.jpg
+Fernando de Augusto Ferreira, 442.987.632-23, augustinhoferreira32@outlook.com, 5697032-3690
+Matheus Antônio de Jesus, 786.321.324-51, matheuszin542@gmail.com, 1196643-2569
+Erica Barros dos Santos, 985.356.576-35, barros.erica45@gmail.com 1195541-8761, 1198810-1847
+José Santos Pinto, 224.764.888-90, josesantospinto5597@gmail.com, 2193327-9064
+Bruno Almeida do Ramos, 259.777.418-79, bruninhoramos34@outlook.com, 3698877-2254,  3696790-0054
+Yuri Rufino de Nunes, 877.037.153-70, yuriGutsLover@hotmail.com, 1191040-1846
+Jogo- A e B, 24/10/2024 15:00
+Jogo B e C 25/10 1700
+Jogo D e E 29/11 10:00
+Jogo E e A 22/01/2024 09:00
+Liberal de Pelotas, hdjdbejd.jpg
+Resenha de Cristal, IMG_20231104.jpg
+Scorpion, IMG_20231205.jpg
+PaySandu, PaySandu_logo.jpg
+Facela F.C., design_facela.jpg
+M19, M19.jpg
+Sete de Julho, sete_logo.jpg
+*/
